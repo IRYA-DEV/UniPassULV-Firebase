414d1264a526e1b640fc2dd0b949ae21ff30111d
IRYA-DEV <iyael117@gmail.com>
2025-01-20 23:49:43 -0600

    Correccion de ruta de credenciales Firebase

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../unipass-ulv-firebase-adminsdk-7z3zf-a48952a665.json' assert { type: 'json' };
+import serviceAccount from '../../unipass-ulv-firebase-adminsdk-7z3zf-a48952a665.json' assert { type: 'json' };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
